f152a1455d813d7342b65dfeb74dcf7950895041
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2020-11-10 15:32:28 +0100

    login, logout and profile

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', views.register, name="register"),
-    path('login/', views.login, name="login"),
+    path('register/', user_views.register, name="register"),
+    path('profile/', user_views.profile, name="profile"),
+    path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,24 +2,27 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 
-def register(request):
-  """Handle users registration"""
-  if request.method == "POST":
-    form = UserRegistrationForm(request.POST)
-    if form.is_valid():
-      form.save()
-      username = form.cleaned_data.get('username')
-      messages.success(request, f'Account created for {username}')
-      return redirect('blog-home')
-  else:
-    form = UserRegistrationForm()
-  return render(request, 'users/register.html', {'form': form})
+def register(request, *args, **kwargs):
+    """Handle users registration"""
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, 'Your Account has been created successfully! you can now login')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
 
 
-def login(request):
-  """Handles users login"""
-  form = AuthenticationForm()
-  return render(request, 'users/login.html', {'form': form})
+
+@login_required
+def profile(request, *args, **kwargs):
+    """Handles users profile"""
+    return render(request, 'users/profile.html')
