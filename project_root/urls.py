4f14d09af7898a3c1a7ec8fde152aab8c7e02ee1
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2020-11-12 02:59:28 +0100

    CRUD Post

--- project_root/urls.py
+++ project_root/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('users/', include('users.urls')),
 ]
 
