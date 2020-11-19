0bf948c85f6a84efb7f1af2162f93e42c51b880a
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2020-11-19 17:22:48 +0100

    hide email

--- project_root/settings.py
+++ project_root/settings.py
@@ -126,5 +126,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ujahemmanuel72@gmail.com'
-EMAIL_HOST_PASSWORD = '85253346'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
